f5616500b117676c4d3054bd02cbb9820f75025a
Tamir <tmelzerb@ucsc.edu>
2022-10-12 17:10:14 -0700

    Changes

--- daytimecli.c
+++ daytimecli.c
@@ -1,7 +1,58 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include <unistd.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define SA struct sockaddr
+#define BUFFER_SIZE 4096
 
 
 int main(int argc, char **argv){
-	printf("Hello world\n");
+	
+	if(argc != 2){
+		fprintf(stderr, "usage: daytimecli [ip address]\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	// Create socket and check if socket function was executed correctly
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(sockfd < 0){
+		fprintf(stderr, "warning: socket error\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//Clear server address struct and set server address properties
+	struct sockaddr_in servaddr;
+	
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(13);
+	
+	if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
+		fprintf(stderr, "warning: could not convert IP address\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	if(connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0){
+		fprintf(stderr, "warning: could not connect to IP address\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	char buffer[BUFFER_SIZE + 1];
+	int n;
+	
+	while((n = read(sockfd, buffer, BUFFER_SIZE)) > 0){
+		buffer[n] = 0; //null terminator
+		write(1, buffer, n);
+	}
+	
+	if(n < 0){
+		fprintf(stderr, "warning: read error\n");
+		exit(EXIT_FAILURE);
+	} 
+	
+	exit(EXIT_SUCCESS);
 }
